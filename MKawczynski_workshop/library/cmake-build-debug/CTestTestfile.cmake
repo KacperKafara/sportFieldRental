b7dd8f14d7ed96ba4d35757c9a545c4b520a1386
Miłosz Kawczyński 242417 <242417@edu.p.lodz.pl>
2022-03-22 18:04:10 +0100

    CMakeLists

---
+++ MKawczynski_workshop/library/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/CLionProjects/pobitest/library
+# Build directory: /home/student/CLionProjects/pobitest/library/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(LibraryTest "Test")
+set_tests_properties(LibraryTest PROPERTIES  _BACKTRACE_TRIPLES "/home/student/CLionProjects/pobitest/library/CMakeLists.txt;70;add_test;/home/student/CLionProjects/pobitest/library/CMakeLists.txt;0;")
