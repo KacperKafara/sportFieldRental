36538e9503bdc18c049ae674650ee3aa5e29fad6
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2022-03-30 08:48:23 +0200

    Deleted .idea and cmake-build-debug directories

--- MKawczynski_workshop/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/CLionProjects/wwan_sr_0830_a/MKawczynski_workshop
-# Build directory: /home/student/CLionProjects/wwan_sr_0830_a/MKawczynski_workshop/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("library")
-subdirs("program")
