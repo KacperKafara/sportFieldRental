2c110dbb0f20ff89b5e38ef0879db03023f812af
Kacper Kafara 242412 <242412@edu.p.lodz.pl>
2022-04-01 19:58:14 +0200

    subpoint 1.4

--- KKafara_workshop/program/src/main.cpp
+++ KKafara_workshop/program/src/main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int main() {
-    Client *client = new Client("Kacper", "Kafara", "242412", NULL);
-    Address *address = new Address("Warsaw", "Batorego", "24");
-    client->setAddress(address);
-    cout << client->getClientInfo() << endl;
-    delete client;
-    cout << address->getAddressInfo() << endl;
+    Address address("Warsaw", "Batorego", "24");
+    Client client1("Kacper", "Kafara", "242412", &address);
+    Client client2("Kacper1", "Kafara1", "131323", &address);
+    Address *adr = client1.getAddress();
+    adr->setCity("Lodz");
+    cout << client1.getClientInfo() << endl;
+    cout << client2.getClientInfo() << endl;
     return 0;
 }
